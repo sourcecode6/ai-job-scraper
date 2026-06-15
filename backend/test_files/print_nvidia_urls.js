5ab2185ca352279399192ccbc927b79df69e123e
Saurabh <mssurashe42@gmail.com>
2026-06-15 21:50:07 +0530

    Removing unnecessary files

--- backend/test_files/print_nvidia_urls.js
+++
@@ -1,2 +0,0 @@
-const { db } = require('../config/db');
-console.log(db.prepare("SELECT job_id, job_title, url, apply_url FROM jobs WHERE company_name = 'NVIDIA' LIMIT 5").all());
--- backend/test_files/print_user_info.js
+++
@@ -1,2 +0,0 @@
-const { db } = require('../config/db');
-console.log(db.prepare("SELECT email, match_threshold, selected_companies FROM users WHERE email = 'mssurashe42@gmail.com'").get());
